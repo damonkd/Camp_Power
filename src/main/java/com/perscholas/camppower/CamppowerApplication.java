9f65801f0094509ddea75222db9c25520b1fc37d
Damon Daniels <damonkd@gmail.com>
2021-08-23 00:46:33 -0700

    project template

---
+++ src/main/java/com/perscholas/camppower/CamppowerApplication.java
@@ -0,0 +1,13 @@
+package com.perscholas.camppower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamppowerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamppowerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/perscholas/camppower/CamppowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.camppower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamppowerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
