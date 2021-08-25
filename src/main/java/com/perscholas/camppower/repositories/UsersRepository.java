bfc2a5445159075b30cffe66912ec236a922cdc8
Damon Daniels <damonkd@gmail.com>
2021-08-24 22:16:46 -0700

    security

--- src/main/java/com/perscholas/camppower/repositories/UsersRepository.java
+++ src/main/java/com/perscholas/camppower/repositories/UsersRepository.java
@@ -2,6 +2,10 @@ package com.perscholas.camppower.repositories;
 
 import com.perscholas.camppower.models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UsersRepository extends JpaRepository<Users, Long> {
+    @Query("SELECT u FROM Users u WHERE u.username = :username")
+    public Users getUserByUsername(@Param("username") String username);
 }
