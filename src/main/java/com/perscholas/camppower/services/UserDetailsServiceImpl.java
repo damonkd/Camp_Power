1a3de89a76eb84616d4f710c34b195a3e4c8ad8b
Damon Daniels <damonkd@gmail.com>
2021-08-24 22:42:01 -0700

    security first draft

---
+++ src/main/java/com/perscholas/camppower/services/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.perscholas.camppower.services;
+
+import com.perscholas.camppower.models.Users;
+import com.perscholas.camppower.repositories.UsersRepository;
+import com.perscholas.camppower.security.MyUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UsersRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        Users user = userRepository.getUserByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Could not find user");
+        }
+
+        return new MyUserDetails(user);
+    }
+
+}
