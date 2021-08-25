06474e93fc2ee000680ac0b2ef0d15df80eec3f1
Damon Daniels <damonkd@gmail.com>
2021-08-24 22:54:51 -0700

    security working

--- src/main/java/com/perscholas/camppower/models/Users.java
+++ src/main/java/com/perscholas/camppower/models/Users.java
@@ -27,9 +27,9 @@ public class Users implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
-    @Email
-    @NotNull
-    String email;
+//    @Email
+//    @NotNull
+//    String email;
 
     @NotNull
     String password;
